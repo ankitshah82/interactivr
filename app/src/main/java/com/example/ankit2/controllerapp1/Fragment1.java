bf0a1f952caf5068783a648b076bbe61387561e1
Ankit <ahs8295@gmail.com>
2017-02-02 01:03:25 +0530

    Added flick gesture detection. Gesture packet reception is now functional.

--- app/src/main/java/com/example/ankit2/controllerapp1/Fragment1.java
+++ app/src/main/java/com/example/ankit2/controllerapp1/Fragment1.java
@@ -9,42 +9,60 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
-
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.Toast;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.UUID;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Vibrator;
 
 /**
  * Created by Ankit on 1/5/2017.
  */
-public class Fragment1 extends Fragment{
+public class Fragment1 extends Fragment implements SensorEventListener {
 
     View myView;
     int reqCode = 1001;
     BroadcastReceiver mReceiver;
+    public Handler mHandler;
     BluetoothAdapter bluetoothAdapter;
-    BluetoothSocket bsock;
+    private float lastX, lastY, lastZ;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+
+
+
+    private boolean startSwipe = true;
+
+    private long lastUpdate = 0;
+
+    private float vibrateThreshold = 0;
+    public Vibrator v;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView = inflater.inflate(R.layout.controller_mode_layout,container,false);
-        ((ControllerActivity)getActivity()).setActionBarTitle("Controller Mode");
+        myView = inflater.inflate(R.layout.controller_mode_layout, container, false);
+        ((ControllerActivity) getActivity()).setActionBarTitle("Controller Mode");
 
-         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
         mReceiver = new BroadcastReceiver() {
@@ -53,15 +71,9 @@ public class Fragment1 extends Fragment{
                 if (intent.getAction().equals("android.bluetooth.devicepicker.action.DEVICE_SELECTED")) {
                     context.unregisterReceiver(this);
                     BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    Toast.makeText(context, "Device" + device.getAddress(), Toast.LENGTH_SHORT).show();
-                    try {
-                        bsock = device.createInsecureRfcommSocketToServiceRecord(UUID.fromString("00002415-0000-1000-8000-00805F9B34FB"));
-                        bsock.connect();
-                        //Send and receive data logic follows
+                    Toast.makeText(context, "Connecting to " + device.getName(), Toast.LENGTH_SHORT).show();
+                    new CommunicationThread(device).start();
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                 }
             }
         };
@@ -71,60 +83,167 @@ public class Fragment1 extends Fragment{
             if (bluetoothAdapter.isEnabled()) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, reqCode);
-                }
-
-                else
-                {
-                    Toast.makeText(getActivity(),"Please select your headset from the list", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), "Please select your headset from the list", Toast.LENGTH_LONG).show();
                     showDevicePicker();
                 }
             }
         }
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
+            // success! we have an accelerometer
 
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+            vibrateThreshold = accelerometer.getMaximumRange() / 2;
+            lastUpdate = System.currentTimeMillis();
+        } else {
+            // fai! we dont have an accelerometer!
+        }
 
+        //initialize vibration
+        v = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
 
 
         return myView;
     }
 
 
-
     @Override
-    public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults)
-    {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
 
-        if((requestCode == reqCode) && (grantResults[0] == PackageManager.PERMISSION_GRANTED))
-        {
-            Toast.makeText(getActivity(),"Please select your headset from the list", Toast.LENGTH_LONG).show();
+        if ((requestCode == reqCode) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            Toast.makeText(getActivity(), "Please select your headset from the list", Toast.LENGTH_LONG).show();
             showDevicePicker();
         }
     }
 
-    public void showDevicePicker()
-    {
+    public void showDevicePicker() {
         //Launch built in bluetooth device picker activity
-        startActivity( new Intent("android.bluetooth.devicepicker.action.LAUNCH")
+        startActivity(new Intent("android.bluetooth.devicepicker.action.LAUNCH")
                 .putExtra("android.bluetooth.devicepicker.extra.NEED_AUTH", false)
                 .putExtra("android.bluetooth.devicepicker.extra.FILTER_TYPE", 0)
-                .putExtra("android.bluetooth.devicepicker.extra.LAUNCH_PACKAGE","com.example.ankit2.controllerapp1")
-                .putExtra("android.bluetooth.devicepicker.extra.DEVICE_PICKER_LAUNCH_CLASS",ControllerActivity.class.getClass())
+                .putExtra("android.bluetooth.devicepicker.extra.LAUNCH_PACKAGE", "com.example.ankit2.controllerapp1")
+                .putExtra("android.bluetooth.devicepicker.extra.DEVICE_PICKER_LAUNCH_CLASS", ControllerActivity.class.getClass())
                 .setFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS));
 
     }
-
 
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         getActivity().unregisterReceiver(mReceiver);
+        sensorManager.unregisterListener(this);
+    }
 
-
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
-       // getActivity().unregisterReceiver(mReceiver);
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
     }
 
 
+    private class CommunicationThread extends Thread {
+        BluetoothSocket bsock;
+        byte messageBytes[];
 
+
+        public CommunicationThread(BluetoothDevice bDevice) {
+
+            try {
+                bsock = bDevice.createInsecureRfcommSocketToServiceRecord(UUID.fromString("00002415-0000-1000-8000-00805F9B34FB"));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            messageBytes = new byte[4];
+            messageBytes[0]=0x7F;
+            messageBytes[1]=0x01;
+            messageBytes[2]=0x06;
+            messageBytes[3]=0x00;
+
+
+        }
+
+        public void run() {
+            Looper.prepare();
+            OutputStream os = null;
+            try {
+                bsock.connect();
+                os = bsock.getOutputStream();
+                os.write(messageBytes);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            final OutputStream bos = os;
+
+            mHandler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    if (bsock.isConnected()) {
+                        messageBytes[3] = (byte) (msg.what);
+                        messageBytes[2] = (byte) (msg.what >>8);
+                        messageBytes[1] = (byte) (msg.what >>16);
+                        messageBytes[0] = (byte) (msg.what >>24);
+
+
+
+                        try {
+
+                            bos.write(messageBytes);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }
+            };
+            Looper.loop();
+
+
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if ((System.currentTimeMillis() - lastUpdate) < 300)
+            return;
+
+
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+
+        double accVectorLen = Math.sqrt(x * x + y * y + z * z);
+        double deltaAcc = Math.abs(accVectorLen - Math.sqrt(lastX * lastX + lastY * lastY + lastZ * lastZ));
+        if (deltaAcc > 10) {
+            if (startSwipe)     //This is the start of movement.
+                startSwipe = false;
+
+            else {
+                startSwipe = true;
+                v.vibrate(50);
+                mHandler.sendEmptyMessage(0x7F010700);
+                lastUpdate = System.currentTimeMillis();
+
+            }
+
+        }
+        lastX = x;
+        lastY = y;
+        lastZ = z;
+
+
+    }
 }
--- app/src/main/java/com/example/ankit2/controllerapp1/Fragment2.java
+++ app/src/main/java/com/example/ankit2/controllerapp1/Fragment2.java
@@ -2,73 +2,80 @@ package com.example.ankit2.controllerapp1;
 
 import android.app.Fragment;
 import android.bluetooth.BluetoothAdapter;
-
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
-
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
-import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresPermission;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
+
 /**
  * Created by Ankit on 1/5/2017.
  */
-public class Fragment2 extends Fragment{
+public class Fragment2 extends Fragment {
 
-     BluetoothAdapter mBluetoothAdapter;
+    BluetoothAdapter mBluetoothAdapter;
     View myView;
     BluetoothSocket socket = null;
-    boolean connected = false;
+
     HandlerThread ht = new HandlerThread("xyz");
     AcceptThread accthread;
-    ReadThread rdthread;
 
-   public  Handler  mHandler;
-    private static final int REQUEST_ENABLE_BT = 10;
+
+    public Handler mHandler;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView = inflater.inflate(R.layout.vr_mode_layout,container,false);
-        ((ControllerActivity)getActivity()).setActionBarTitle("VR Mode");
+        myView = inflater.inflate(R.layout.vr_mode_layout, container, false);
+        ((ControllerActivity) getActivity()).setActionBarTitle("VR Mode");
 
         ht.start();
         mHandler = new Handler(ht.getLooper()) {
             @Override
             public void handleMessage(Message inputMessage) {
 
-                TextView  tv = (TextView) getView().findViewById(R.id.textView3);
-                tv.setText("Received Message");
+
+
+                if ((inputMessage.what&0x0000FF00) == 0x0600)
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            TextView tv = (TextView) myView.findViewById(R.id.textView3);
+                            tv.setText("");
+                            tv = (TextView) myView.findViewById(R.id.textView2);
+                            tv.setText("Controller connected ");
+
+                        }
+                    });
+
+                else if((inputMessage.what&0x0000FF00) == 0x0700)
+                    Toast.makeText(getActivity(), "Flick gesture detected", Toast.LENGTH_LONG).show();
 
 
             }
 
+
         };
-         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
         Intent discoverableIntent =
                 new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
         startActivity(discoverableIntent);
-
-
 
 
         accthread = new AcceptThread();
@@ -86,11 +93,9 @@ public class Fragment2 extends Fragment{
             BluetoothServerSocket tmp = null;
             try {
                 // MY_UUID is the app's UUID string, also used by the client code.
-               // Toast.makeText(getActivity(), "listening", Toast.LENGTH_SHORT).show();
                 tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("BT_SERVER",
                         UUID.fromString("00002415-0000-1000-8000-00805F9B34FB"));
             } catch (IOException e) {
-               // Toast.makeText(getActivity(), "listen exception", Toast.LENGTH_SHORT).show();
             }
             mmServerSocket = tmp;
         }
@@ -99,33 +104,31 @@ public class Fragment2 extends Fragment{
 
 
             // Keep listening until exception occurs or a socket is returned.
-            while (!connected) {
+
+            try {
+
+                socket = mmServerSocket.accept();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+
+            }
+
+            if (socket != null) {
+                new ReadThread().start();
                 try {
-
-                    socket = mmServerSocket.accept();
-
-
-                } catch (Exception e) {
+                    mmServerSocket.close();
+                } catch (IOException e) {
                     e.printStackTrace();
-                    break;
-
-                }
-
-                if(socket != null)
-                {
-                    new ReadThread().start();
-                    try {
-                        mmServerSocket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-
-                }
                 }
 
 
             }
+
+
+        }
 
         public void cancel() {
             try {
@@ -134,51 +137,60 @@ public class Fragment2 extends Fragment{
                 e.printStackTrace();
             }
         }
-        }
+    }
 
     class ReadThread extends Thread {
 
+        byte[] mmBuffer = new byte[4];
 
-        public ReadThread() {
-
-        }
 
         public void run() {
-           // Toast.makeText(getActivity(), "read thread started", Toast.LENGTH_SHORT).show();
 
 
-            while(true) {
-                if(connected)
+
+            while (socket.isConnected()) {
+
                 try {
-                    if (socket != null) {
 
-                        InputStream tmpIn;
-                        byte[] mmBuffer = new byte[3];
-                        tmpIn = socket.getInputStream();
 
-                        tmpIn.read(mmBuffer);
-                        mHandler.sendEmptyMessage(5);
+                    InputStream tmpIn = socket.getInputStream();
 
-                        String readString = new String(mmBuffer);
-                        socket.close();
-                        break;
-                    }
+                    tmpIn.read(mmBuffer);
 
+                    Message msg = Message.obtain();
+
+
+                    msg.obj = new String(mmBuffer);
+                    msg.what = mmBuffer[0];
+                    msg.what = msg.what << 8;
+                    msg.what += mmBuffer[1];
+                    msg.what = msg.what << 8;
+                    msg.what += mmBuffer[2];
+                    msg.what = msg.what << 8;
+                    msg.what += mmBuffer[3];
+
+                    mHandler.sendMessage(msg);
 
 
                 } catch (IOException e) {
-                    //do something
+                    e.printStackTrace();
 
                 }
+
             }
         }
     }
 
-        // Closes the connect socket and causes the thread to finish.
-        public void cancel() {
-
+    // Closes the connect socket and causes the thread to finish.
+    public void cancel() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
+}
 
 
 
